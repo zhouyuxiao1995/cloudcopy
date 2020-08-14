39c6c9c1bf3e38488578a5d2617b8874b1e4bea1
zhouyuxiao <940793261@qq.com>
2020-08-14 15:26:19 +0800

    生成流程图

--- ant-design-vue-jeecg/src/components/layouts/UserLayout.vue
+++ ant-design-vue-jeecg/src/components/layouts/UserLayout.vue
@@ -5,11 +5,11 @@
         <div class="header">
           <a href="/">
             <img src="~@/assets/logo.svg" class="logo" alt="logo">
-            <span class="title">Jeecg Boot</span>
+            <span class="title">云抄袭</span>
           </a>
         </div>
         <div class="desc">
-          Jeecg Boot 是中国最具影响力的 企业级 快速开发平台
+          云抄袭是取大家之精华 去其糟粕 企业级 快速开发平台
         </div>
       </div>
 
@@ -22,7 +22,7 @@
           <a href="https://github.com/zhangdaiscott/jeecg-boot/blob/master/LICENSE" target="_blank">条款</a>
         </div>
         <div class="copyright">
-          Copyright &copy; 2019 <a href="http://www.jeecg.com" target="_blank">JEECG开源社区</a> 出品
+          Copyright &copy; 2019 <a href="http://www.baidu.com" target="_blank">云抄袭开源社区</a> 出品
         </div>
       </div>
     </div>
